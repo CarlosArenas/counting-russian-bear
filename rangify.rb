bb362445063a8bb914143be0595170bc81a2e34d
Carlos Arenas <carlos.arenas@gmail.com>
2013-03-04 08:42:38 -0500

    Code layout cleanup

--- rangify.rb
+++ rangify.rb
@@ -27,7 +27,7 @@ class BatesNumber
   end
 end
 
-Document = Struct.new(:first_page, :last_page)
+Document   = Struct.new(:first_page, :last_page)
 BatesRange = Struct.new(:first_page, :last_page)
 
 # read list of documents
@@ -43,7 +43,9 @@ CSV.foreach(ARGV[0], headers: true) do |row|
   custodian = '[na]'    if custodian.nil? or custodian.empty?
 
   # track row as custodian's document
-  docs[custodian] << Document.new( BatesNumber.new(beg_bates), BatesNumber.new(end_bates))
+  docs[custodian] << Document.new(
+    BatesNumber.new(beg_bates), BatesNumber.new(end_bates)
+  )
 end
 
 # write custodian ranges
@@ -55,25 +57,27 @@ CSV(STDOUT, force_quotes: true ) do |csv|
     range  = BatesRange.new
 
     # start range with first doc
-    first_doc = documents.shift
+    first_doc        = documents.shift
     range.first_page = first_doc.first_page
     range.last_page  = first_doc.last_page
 
     # continue rangifying remaining docs
     documents.each do |doc|
-      if range.last_page.next == doc.first_page.to_s
-        range.last_page = doc.last_page
+      if range.last_page.next == doc.first_page.to_s  # hack: compare as str
+        range.last_page  = doc.last_page
       else
         ranges << range
-        range = BatesRange.new
+        range            = BatesRange.new
         range.first_page = doc.first_page
         range.last_page  = doc.last_page
       end
     end
 
     # report ranges
-    ranges << range
-    ranges.each { |range| csv <<  [custodian, range.first_page, range.last_page] }
+    ranges << range   # add last range
+    ranges.each do |range|
+      csv << [custodian, range.first_page, range.last_page]
+    end
 
   end
 end
